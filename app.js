a1cd66a034fd8957713d5fc46ad0b541efdac461
Alexander Andreyev <alexandr210993@gmail.com>
2018-03-03 23:37:26 +0200

    Added swagger

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const index = require('./routes/index');
 const users = require('./routes/users');
 
@@ -19,17 +22,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
